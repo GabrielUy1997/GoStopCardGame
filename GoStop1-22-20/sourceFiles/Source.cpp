aa3983ff2eecd937e70430e20195c10681a26523
gabriel uy <guy@ramapo.edu>
2020-03-09 13:42:52 -0400

    Documentation Update

--- GoStop1-22-20/sourceFiles/Source.cpp
+++ GoStop1-22-20/sourceFiles/Source.cpp
@@ -7,7 +7,21 @@
 //* Class : CMPS 147-01 OPL                                  *
 //* Date : 3/10/20                                           *
 //************************************************************
+
+/**********************************************************************
+Function Name: main
+Purpose: Main entry point for program
+Parameters:
+		None
+Return Value: int exit 
+Local Variables:
+		None
+Algorithm:
+		1) Create the Game object
+Assistance Received: None
+**********************************************************************/
 int main()
 {
 	Game startGame;
+	return 0;
 }
--- GoStop1-22-20/sourceFiles/headers/headers.h
+++ GoStop1-22-20/sourceFiles/headers/headers.h
@@ -9,7 +9,21 @@
 #include <cstdlib>
 #include <fstream>
 
-inline void dashedLine() //Edwin O'Connor suggested I use this to seperate output and make it more readable 1/23/20
+/**********************************************************************
+Function Name: dashedLine
+Purpose: Used to seperate different parts in the UI and make it more
+readable
+Parameters:
+		None
+Return Value: None
+Local Variables:
+		None
+Algorithm:
+		1) Prints a line
+Assistance Received: Edwin O'Connor suggested I use this to seperate 
+output and make it more readable 1/23/20
+**********************************************************************/
+inline void dashedLine() 
 {
 	std::cout << "----------------------------------------------------------\n";
 }
